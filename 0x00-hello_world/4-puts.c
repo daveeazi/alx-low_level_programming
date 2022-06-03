4afbc2c33c721f26dfa0c3721a767192290ab450
Daveeazi <davidatat8@gmail.com>
2022-06-03 07:12:54 -0700

    Hello, puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- *
- *  * main - function to print out a simplee text
- *
- *   * Return:- zero is returned
- *
- *    */
-
+ * main - main block
+ * Return: 0
+ */
 int main(void)
-
 {
-
-		/*Print out some textsc*/
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
