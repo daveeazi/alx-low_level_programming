44c98acfe3c1fe51bdfca0256f9dee7f59c7410f
Daveeazi <davidatat8@gmail.com>
2022-06-03 14:40:02 -0700

    Positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random 
+ * number stored in the variable n
+ * Reyrun: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
+
