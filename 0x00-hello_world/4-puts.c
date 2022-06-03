1e51f57b1f045f5f951e139531d80d51303e86b0
Daveeazi <davidatat8@gmail.com>
2022-06-03 06:51:33 -0700

    Hello, puts 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- * /
+ *  * main - function to print out a simplee text
+ *
+ *   * Return:- zero is returned
+ *
+ *    */
+
 int main(void)
+
 {
-	puts ("\" Programming is like building a multilingual puzzle");
-	return (0);
+
+		/*Print out some textsc*/
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
