16ac9fd45c889a7637bc080ee1acb4e0574735d5
Daveeazi <davidatat8@gmail.com>
2022-06-05 14:22:35 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - start of execution
+ *
+ *   * Return: zero
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		char index;
+
+
+
+			for (index = '0'; index <= '9'; index++)
+
+					{
+
+								putchar(index);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+
+
+}
