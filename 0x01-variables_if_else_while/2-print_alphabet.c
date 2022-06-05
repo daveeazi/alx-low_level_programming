5a25c8204505be610c24c3f364ad6d071e0b1750
Daveeazi <davidatat8@gmail.com>
2022-06-05 14:07:28 -0700

    Program prints lowercased alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - start of execution
+ *
+ *   * Return: zero
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		char index;
+
+
+
+			for (index = 'a'; index <= 'z'; index++)
+
+					{
+
+								putchar(index);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+
+
+}
