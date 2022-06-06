96ed1cb70cf2c9a32cfb763086ab1db3ebf1bae1
Daveeazi <davidatat8@gmail.com>
2022-06-06 09:04:04 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,16 +2,17 @@
 
 /**
  * main - start of execution
- * 
  * Return: zero
  */
 
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i <= 8; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
+
 		{
 			putchar(i % 10 + '0');
 			putchar(j % 10 + '0');
