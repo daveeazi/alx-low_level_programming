0abb7bfec8282448fa586e3eaffdda3f759edc79
Daveeazi <davidatat8@gmail.com>
2022-06-09 06:36:10 -0700

    I commented the part causing an infinte loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinte loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ *
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
