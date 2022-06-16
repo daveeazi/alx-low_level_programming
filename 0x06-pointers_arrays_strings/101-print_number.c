3fd121534f05d7a0804add2f400d9c6fcdd5441c
Daveeazi <davidatat8@gmail.com>
2022-06-16 08:57:56 -0700

    Test 1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
- */
-void print_number(int n)
+int main(void)
 {
-unsigned int m, d, count;
+  int n;
+  int a[5];
+  int *p;
 
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
-
-d = m;
-count = 1;
-
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  p[5] = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
