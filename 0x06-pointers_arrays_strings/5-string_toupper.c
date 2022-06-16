72d340442b8285b139c1bf9c1cc5f4c19dfec30b
Daveeazi <davidatat8@gmail.com>
2022-06-16 08:05:01 -0700

    A function that changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase 
+ * letters of a string to uppercase
+ * @c: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 'a' - 'A';
+	}
+	return (c);
+}
