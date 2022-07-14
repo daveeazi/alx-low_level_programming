eccda1ac1adf8043677a3459fa390ccf533e0727
David <davidatat8@gmail.com>
2022-07-14 12:47:16 -0700

    Function prints char with amin function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
+#include <stdio.h>
+
+void __attribute__((construct)) hare(void);
 
 /**
- * start up func - executes before main function is executed.
- * Return: nothing
+ * hare - Prints a string before the main function is executed
  */
-
-void startupfunc(void) __attribute__ ((constructor));
-void startupfunc(void)
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
